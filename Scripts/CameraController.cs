a9c321bed966f3b24b5004673a4ba5a14991ef7f
NameSsis <rudenko_lex@mail.ru>
2025-04-29 20:31:59 +0300

    1231231212312312

--- Scripts/CameraController.cs
+++ Scripts/CameraController.cs
@@ -4,37 +4,17 @@ public class CameraController : MonoBehaviour {
 
     [SerializeField] private Transform player;
     [SerializeField] private float cameraSpeed = 1.0f;
-    [SerializeField] private float xOffsetParam = 0.0f;
-    [SerializeField] private float yOffsetParam = 0.0f;
-
-    private Rigidbody2D heroRb;
 
     private Vector3 pos;
-    private float xOffset = 0.0f;
-    private float yOffset = 0.0f;
 
     private void Awake() {
         if (!player){
-            player = FindAnyObjectByType<CharacterController>().transform;
+            player = FindAnyObjectByType<MainHero>().transform;
         }
-
-        heroRb = player.GetComponent<Rigidbody2D>();
     }
     
     void Update() {
-
-        if (player.GetComponent<CharacterController>().moveInput > 0) 
-            xOffset = xOffsetParam;
-        else if (player.GetComponent<CharacterController>().moveInput < 0)
-            xOffset = xOffsetParam * (-1);
-
-        if (Input.GetAxis("Vertical") > 0) 
-            yOffset = yOffsetParam;
-        else if (Input.GetAxis("Vertical") < 0)
-            yOffset = yOffsetParam * (-1);
-        
-
-        pos = player.position  + new Vector3(xOffset, yOffsetParam, 0);
+        pos = player.position;
         pos.z = -10f;
 
         if (cameraSpeed < 0.1f) {
